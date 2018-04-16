f1392ba4402e287c4294ff0d5f2c1ebe4d813659
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-04-16 22:23:26 +0200

    Initial revision

---
+++ test/basic.test.js
@@ -0,0 +1,27 @@
+const test = require('tap').test
+const mockRequire = require('mock-require')
+const mockFs = require('mock-fs')
+const dotenv = require('../lib/')
+
+
+test('basic test', t => {
+  t.test('will use default hierarchy of files', t => {
+    mockFs({
+      '.env.production': 'test',
+      '.env.local': 'test',
+      '.env': 'test'
+    })
+    let files = []
+    let calls = 0
+    mockRequire('dotenv', { config: c => files.push(c.path) })
+    mockRequire('dotenv-expand', () => calls++)
+
+    dotenv()
+
+    t.plan(2)
+    t.equals(calls, 3)
+    t.same(files, ['.env.production', '.env.local', '.env'])
+  })
+
+  t.end()
+})
