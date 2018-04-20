2c23f1d3168ea8ac2deb6e0909052a82375c2530
Andrzej Lichnerowicz <andrzej@lichnerowicz.pl>
2018-04-20 20:57:59 +0200

    Add test for multiple custom files to load

--- test/common.test.js
+++ test/common.test.js
@@ -17,5 +17,41 @@ test('in any environment', t => {
     t.same(files, ['.env.production', '.env.local'])
   })
 
+  t.test('will only load a single specified file instead of default', t => {
+    mockFs({
+      '.env.production': 'test',
+      '.env.local': 'test',
+      '.env': 'test',
+      '.env.different': 'test'
+    })
+
+    const {calls, files} = mockRequire(() => dotenv({
+      env: '.env.different'
+    }))
+
+    t.plan(2)
+    t.equals(calls, 1)
+    t.same(files, ['.env.different'])
+  })
+
+  t.test('will only load list of specified files instead of default', t => {
+    mockFs({
+      '.env.production': 'test',
+      '.env.local': 'test',
+      '.env': 'test',
+      '.env.different': 'test',
+      '.env.different.local': 'test',
+      '.env.moar': 'test'
+    })
+
+    const {calls, files} = mockRequire(() => dotenv({
+      env: ['.env.different', '.env.different.local', '.env.moar']
+    }))
+
+    t.plan(2)
+    t.equals(calls, 3)
+    t.same(files, ['.env.different', '.env.different.local', '.env.moar'])
+  })
+
   t.end()
 })
